034932bac8e8091bc66e8ef0666147e95a4191e0
yijung1993 <yijung1993@gmail.com>
2026-06-04 22:39:58 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budget-v5';
+const CACHE = 'budget-v6';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png',
   './icons/home.png','./icons/calendar.png','./icons/assets.png','./icons/stats.png',
   './icons/history.png','./icons/insurance.png','./icons/settings.png'];
